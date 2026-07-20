8f90aed6e2ce59ca8dd5e1ec8c7e2f9486dbbf97
RamyaJarugulla7 <ramya.jarugulla@techdenali.com>
2026-07-20 14:18:11 +0530

    Resolved lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import "@rentbook/rentbook-ui-lib/microfrontend.min.css";
 import OrderDetails from './pages/OrderDetails';
-import OrderBookDetails from './pages/OrderBookDetails';
+
 function App() {
   return (
     <>
