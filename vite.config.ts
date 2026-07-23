20ae9be1fcf463aca7cc0e83885b60107f455b89
sunayana-lakki <sunayana.lakki@techdenali.com>
2026-07-23 17:09:11 +0530

    add vitest configurations to the application and ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vitest/config";
 import react from '@vitejs/plugin-react'
 import path from "path";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  cssInjectedByJsPlugin()],
-
+  plugins: [react(), cssInjectedByJsPlugin()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.widget.tsx"),
